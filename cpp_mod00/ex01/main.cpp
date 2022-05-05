b5235d55546739c49ef2dca4be23d19e4e2699ca
denicem <denicem@hotmail.de>
2022-05-05 04:18:16 +0200

    added main.cpp to test classes.

---
+++ cpp_mod00/ex01/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/05 04:11:25 by dmontema          #+#    #+#             */
+/*   Updated: 2022/05/05 04:11:25 by dmontema         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.h"
+#include <iostream>
+
+int main()
+{
+	Contact c0 = Contact("DNC", "M", "1234", "Pssht!");
+	// c0.set_first("Denice");
+	std::cout << c0.get_first() << " " << c0.get_last() << " : " << c0.get_number() << " | " << c0.get_secret() << std::endl;
+	return (0);
+}
