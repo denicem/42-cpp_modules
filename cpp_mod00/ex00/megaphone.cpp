a2e8243d3c63de1482f4ab713495d26303849ca5
denicem <denicem@hotmail.de>
2022-05-05 03:43:00 +0200

    made code clearer. rm 'using namespace ...'

--- cpp_mod00/ex00/megaphone.cpp
+++ cpp_mod00/ex00/megaphone.cpp
@@ -12,23 +12,25 @@
 
 #include <iostream>
 
-using namespace std;
-
-static int	toUpper(int c) {
+static int to_Upper(int c)
+{
 	if (c >= 'a' && c <= 'z')
-		c = 'A' + c - 'a';
+		c = (c - 'a') + 'A';
 	return (c);
 }
 
 int	main(int argc, char const *argv[])
 {
-	int	i = 1;
-	for (; argv[i]; i++) {
-		for (; *argv[i]; argv[i]++)
-			cout << (char) toUpper(*argv[i]);
-		if (!(*(argv[i] + 1)))	
-			cout << " ";
+	if (argc > 1)
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << static_cast<char>(to_Upper(argv[i][j])); // normal type casting allowed?
+		}
+		std::cout << std::endl;
 	}
-	cout << endl;
-	return 0;
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
 }
