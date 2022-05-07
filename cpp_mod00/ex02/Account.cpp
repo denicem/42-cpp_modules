3fcdfe3d0d50b804548b1fd9cb06a9b0a5ea4dbc
Denice Montemayor <dmontema@1-D-10.42heilbronn.de>
2022-05-07 21:06:02 +0200

    ex02: implemented missing funcs from class Account.

--- cpp_mod00/ex02/Account.cpp
+++ cpp_mod00/ex02/Account.cpp
@@ -12,3 +12,58 @@
 
 #include "Account.hpp"
 
+int Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+	this->_accountIndex = _nbAccounts++;
+	_totalAmount += this->_amount;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+}
+
+void Account::makeDeposit(int deposit)
+{
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	if (this->checkAmount() > 0 && (_amount - withdrawal) >= 0)
+	{
+		this->_amount -= withdrawal;
+		this->_nbWithdrawals++;
+		_totalAmount -= withdrawal;
+		_totalNbWithdrawals++;
+		return (true);
+	}
+	return (false);
+}
+
+int Account::checkAmount() const
+{
+	return (this->_amount);
+}
+
