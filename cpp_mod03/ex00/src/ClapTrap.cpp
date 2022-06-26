369b2ae90a0810962c06ae616d32f763c5d1e791
Denice Montemayor <dmontema@1-D-13.42heilbronn.de>
2022-06-26 18:38:42 +0200

    cpp_mod03: finished ex00.

--- cpp_mod03/ex00/src/ClapTrap.cpp
+++ cpp_mod03/ex00/src/ClapTrap.cpp
@@ -6,13 +6,18 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 18:19:31 by dmontema          #+#    #+#             */
-/*   Updated: 2022/06/13 18:19:31 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/06/26 18:30:32 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/ClapTrap.h"
 
-ClapTrap::ClapTrap(): name("NONAME"), hp(10), ep(10), ad(0) {}
+#include <iostream>
+
+ClapTrap::ClapTrap(): name("NONAME"), hp(10), ep(10), ad(0)
+{
+	std::cout << "ClapTrap NONAME created (Default).\n";
+}
 
 ClapTrap::ClapTrap(const ClapTrap& other)
 {
@@ -28,6 +33,41 @@ ClapTrap & ClapTrap::operator=(const ClapTrap &other) //TODO: implement GETTER &
 	return (*this);
 }
 
-ClapTrap::~ClapTrap() {}
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap " << this->name << " destroyed.\n";
 
-ClapTrap::ClapTrap(std::string name): name(name), hp(10), ep(10), ad(0) {}
+}
+
+ClapTrap::ClapTrap(std::string name): name(name), hp(10), ep(10), ad(0)
+{
+	std::cout << "ClapTrap " << name << " created.\n";
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (this->hp && this->ep)
+	{
+		int damage = 1;
+		this->ep--;
+		std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << damage << " point of damage!\n";
+	}
+}
+
+void ClapTrap::takeDamage(unsigned int damage)
+{
+	if (this->hp)
+	{
+		this->hp -= damage;
+		std::cout << "ClapTrap " << this->name << " took " << damage << " damage!\n";
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int rp)
+{
+	if (this->hp)
+	{
+		this->hp += rp;
+		std::cout << "ClapTrap " << this->name << " repaired itself by " << rp << " hp!\n";
+	}
+}
