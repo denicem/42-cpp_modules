1cc709175fa3da4b1fb9bdc1a697d17793e2670d
Denice Montemayor <dmontema@1-D-13.42heilbronn.de>
2022-08-10 17:07:24 +0200

    cpp-mod08 - ex00: finished.

--- cpp_mod08/ex00/inc/easyfind.h
+++ cpp_mod08/ex00/inc/easyfind.h
@@ -6,7 +6,7 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 04:34:23 by dmontema          #+#    #+#             */
-/*   Updated: 2022/08/09 04:34:23 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/08/10 16:47:09 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,24 @@
 #define EASYFIND_H
 
 #include <algorithm>
+#include <exception>
+
+class NotFoundException: public std::exception
+{
+	const char* what() const throw() {
+		return ("Not Found.");
+	}
+};
 
 template<typename T>
-void easyfind(T t, int i)
+T easyfind(std::vector<T>& t, int i)
 {
-	
+	for (typename std::vector<T>::iterator it = t.begin(); it != t.end(); it++)
+	{
+		if (*it == i)
+			return (*it);
+	}
+	throw NotFoundException();
 }
 
 #endif
--- cpp_mod08/ex00/src/main.cpp
+++ cpp_mod08/ex00/src/main.cpp
@@ -6,17 +6,36 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 04:38:06 by dmontema          #+#    #+#             */
-/*   Updated: 2022/08/09 04:38:06 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/08/10 17:03:10 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/easyfind.h"
 
 #include <iostream>
+#include <vector>
 
 int main()
 {
+	std::vector<int> nbrs;
+	nbrs.push_back(56);
+	nbrs.push_back(52);
+	nbrs.push_back(17);
+	nbrs.push_back(20);
+	nbrs.push_back(30);
+	nbrs.push_back(1);
+	nbrs.push_back(4);
 
+	try
+	{
+		int found = easyfind<int>(nbrs, 20);
+		// int found = easyfind<int>(nbrs, 45);
+		std::cout << found << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
 	return (0);
 }
