7d253cbbdf58420b6fb1e4da1a2c3c6846ceed76
Denice Montemayor <dmontema@1-D-13.42heilbronn.de>
2022-08-12 12:42:45 +0200

    cpp_mod08 - ex02: added compare tester (std::list).

--- cpp_mod08/ex02/src/main.cpp
+++ cpp_mod08/ex02/src/main.cpp
@@ -6,13 +6,37 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 22:33:25 by dmontema          #+#    #+#             */
-/*   Updated: 2022/08/11 00:41:45 by dmontema         ###   ########.fr       */
+/*   Updated: 2022/08/12 12:40:41 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/MutantStack.h"
 
 #include <iostream>
+#include <list>
+
+void func()
+{
+	std::list<int> list;
+	list.push_back(5);
+	list.push_back(17);
+	std::cout << "top " << list.back() << std::endl;
+	list.pop_back();
+	std::cout << "size " << list.size() << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737); //[...] list.push_back(0);
+	std::list<int>::iterator it = list.begin();
+	std::list<int>::iterator ite = list.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::list<int> l(list);
+}
 
 int main()
 {
@@ -35,5 +59,7 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << "LIST:" << std::endl;
+	func();
 	return 0;
 }
